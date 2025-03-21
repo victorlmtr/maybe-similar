30050d3e700dc7d2fc69d32e52e4f48d0d4ef0b6
victorlmtr <victor.lemaitre1@gmail.com>
2025-03-21 06:30:42 +0100

    Fetch song info with youtube api key

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,7 @@ console.log("DB_USER:", process.env.DB_USER);
 console.log("DB_HOST:", process.env.DB_HOST);
 console.log("DB_NAME:", process.env.DB_NAME);
 console.log("DB_PORT:", process.env.DB_PORT);
+console.log("YOUTUBE_API_KEY:", process.env.YOUTUBE_API_KEY);
 
 // PostgreSQL connection
 const pool = new Pool({
--- backend/youtubeService.js
+++ backend/youtubeService.js
@@ -4,8 +4,10 @@ const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
 
 const getVideoDetails = async (videoId) => {
   const url = `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${YOUTUBE_API_KEY}&part=snippet,contentDetails`;
+  console.log(`Fetching details for video ID: ${videoId}`);
   const response = await fetch(url);
   const data = await response.json();
+  console.log(`Response data for video ID ${videoId}:`, JSON.stringify(data));
 
   if (data.items && data.items.length > 0) {
     const video = data.items[0];
